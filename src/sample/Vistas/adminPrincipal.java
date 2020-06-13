6848307e4a4d09befacc2b02fa604f97d2c2c46d
Litzy Mariana <17030609@itcelaya.edu.mx>
2020-06-13 02:40:25 -0500

    archivos

---
+++ src/sample/Vistas/adminPrincipal.java
@@ -0,0 +1,44 @@
+package sample.Vistas;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class adminPrincipal extends Stage {
+
+    private Scene escena;
+    private BorderPane contenedor;
+    private Label lblTacos, lblBebidas, lblEmpleados, lblRegistros;
+    private Button btnSalir;
+    private ToolBar tlbmenu;
+
+    public adminPrincipal(){
+
+        CrearGUI();
+        this.setTitle("Administrador");
+        this.setScene(escena);
+        this.show();
+    }
+
+    private void CrearGUI(){
+
+        tlbmenu = new ToolBar();
+        btnSalir = new Button();
+        tlbmenu.getItems().add(btnSalir);
+        btnSalir.setOnAction(event -> salir());
+        contenedor = new BorderPane();
+        contenedor.setTop(tlbmenu);
+        escena = new Scene(contenedor);
+
+    }
+
+    private void salir(){
+
+    }
+
+
+}
---
+++ src/sample/conexion.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class conexion {
+
+    //volvemos constantes las bariables con final
+    private static final String host = "localhost";
+    private static final String  user = "root";
+    private static final String pwd = "Topicos123";
+    private static final String db = "taqueria";
+
+    public static Connection con;
+
+    public static void crearConexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //se abre el socket hacia el SGBD
+            con = DriverManager.getConnection("jdbc:mysql://"+host+":3306/"+db+"?useSSL=false",user,pwd);
+            System.out.println("se ha iniciado la conexion");
+        } catch (Exception e ) {
+            e.printStackTrace();
+        }
+    }
+}
